da1df67c429b72d8809b8bc1d6d20b16529a0a41
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-05-17 23:26:43 +0700

    api register

--- server/app.js
+++ server/app.js
@@ -1,9 +1,41 @@
 // Add the root project directory to the app module search path
 require('app-module-path').addPath(__dirname);
-// const api = require('./routes/api');
+const api = require('./routes/api');
 // const download = require('./routes/download');
 const path = require('path');
 
 module.exports = function (app) {
-    // app.use()
+    app.use("/api", api);
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(
+            __dirname, '../public/index.html'
+        ))
+    });
+
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+        app.use((err, req, res, next) => {
+            if (err.errors) {
+                res.json({ error: true, errors: err.errors, message: err.message });
+            } else {
+                res.send(err.stack);
+            }
+        });
+    }
+
+    // production erro handler
+    // no stacktraces leadked to user
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        if (err.errors && Object.keys(err.errors).length) {
+            res.json({
+                error: true,
+                errors: err.errors || {}
+            });
+        } else {
+            res.send(err.message);
+        }
+    })
 }
--- server/routes/api/user/index.js
+++ server/routes/api/user/index.js
@@ -9,7 +9,7 @@ const signupValidatior = require("../../../shared/validations/register");
  * @desc Check if user is logged in
  * @access Public
 */
-router.get('/', verifyToken, async (req, res) => {
+router.post('/register', verifyToken, async (req, res) => {
 	try {
 		const user = await User.findById(req.userId).select('-password')
 		if (!user)
@@ -26,51 +26,35 @@ router.get('/', verifyToken, async (req, res) => {
  * @desc Register user
  * @access Public
 */
-router.post('/register', async (req, res) => {
+router.post('/register', (req, res, next) => {
 	const { username, password } = req.body
-	const {isValid, errors} = signupValidatior(req.body);
-	
-	if(!isValid)
-	// Simple validation
-	if (!username || !password)
-		return res
-			.status(400)
-			.json({ success: false, message: 'Missing username and/or password' })
+	const { isValid, errors } = signupValidatior(req.body);
 
-	try {
-		// Check for existing user
-		const user = await User.findOne({ username })
-
-		if (user)
-			return res
-				.status(400)
-				.json({ success: false, message: 'Username already taken' })
-
-		// All good
-		const hashedPassword = await argon2.hash(password)
-		const newUser = new User({ username, password: hashedPassword })
-		await newUser.save()
-
-		// Return token
-		const accessToken = jwt.sign(
-			{ userId: newUser._id },
-			process.env.ACCESS_TOKEN_SECRET
-		)
-
-		res.json({
-			success: true,
-			message: 'User created successfully',
-			accessToken
-		})
-	} catch (error) {
-		console.log(error)
-		res.status(500).json({ success: false, message: 'Internal server error' })
+	if (!isValid) {
+		return res.status(400).json({ error: true, errors });
 	}
-})
 
-// @route POST api/auth/login
-// @desc Login user
-// @access Public
+	co(function* () {
+		const existingUser = yield User.findOne({ username });
+
+		if (existingUser) {
+			const error = { status: 400, errors: { username: 'Username already exist' } }
+			throw error;
+		}
+
+		const user = new User({ username, password });
+		return user.save();
+	}).then(user => res.json({
+		username: user.username,
+		access_token: user.access_token
+	})).catch(err => next(err));
+});
+
+/**
+ * @route POST api/auth/login
+ * @desc Login user
+ * @access Public
+*/
 router.post('/login', async (req, res) => {
 	const { username, password } = req.body
 
