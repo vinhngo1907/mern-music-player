7256c0d8911497e5ab702940a73be791fd61356f
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-05-20 20:31:18 +0700

    add file PageScraper

---
+++ server/lib/PageScraper.js
@@ -0,0 +1,20 @@
+const BaseScraper = require('./BaseScraper');
+const util = require('util');
+
+/**
+ * @description model
+ *  {
+      "songs": [
+        {
+          "title": "What do you mean?",
+          "id": "69797",
+          "artist": "Justin Bieber"
+        },
+        {
+          "title": "We don't talk any more",
+          "id": "69797",
+          "artist": "Charlie Purth
+        },
+      ]
+    }
+ */
