fe3a8903bf4099c2cc61b867a9ef4c1ae5b4baee
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-05-22 23:01:16 +0700

    store

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import authReducer from "./auth_reducer";
 
-
 export default combineReducers({
     auth: authReducer,
     routing: routerReducer
--- app/store.js
+++ app/store.js
@@ -8,9 +8,26 @@ let middleware = [thunk];
 // apply logger middleware in the development environment
 
 if (process.env.NODE_ENV !== 'production') {
-  const logger = require('./logger').default;
+    const logger = require('./logger').default;
 
-  middleware = [...middleware/* , logger */];
+    middleware = [...middleware/* , logger */];
 }
 
 const queueFromLocalStorage = loadQueueState();
+const persistedData = {
+    queueState: queueFromLocalStorage,
+    auth: {
+        authenticated: Boolean(loadUserData()),
+        user: loadUserData(),
+        errors: {},
+    },
+};
+
+const store = createStore(rootReducer, persistedData, applyMiddleware(...middleware));
+
+store.subscribe(_throttle(() => {
+    saveQueueState(store.getState());
+}, 1000 * 60 * 5));
+// save songs in queue to the localStorage every 1 minutes
+
+export default store;
