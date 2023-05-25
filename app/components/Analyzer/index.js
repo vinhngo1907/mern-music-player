e17740b07a4dceae314525a1b6bcac2a0c37d013
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-05-25 20:10:46 +0700

    analyzer component

--- app/components/Analyzer/index.js
+++ app/components/Analyzer/index.js
@@ -1,3 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./index.sass";
+
+const Analyzer = ({ show }) => {
+    <div id="analyzer" style={{ display: show ? "block" : "none" }}>
+        <canvas id="analyzer_render" />
+    </div>
+}
+
+Analyzer.propTypes = {
+    show: PropTypes.bool.isRequired
+}
+
+export default Analyzer;
--- app/containers/App.js
+++ app/containers/App.js
@@ -5,41 +5,41 @@ import { Nav, Analyzer } from "../components";
 import * as Containers from "./";
 
 class App extends React.Component {
-  render() {
-    const {
-      showPlayer,
-      showAnalyzer: show,
-      showQueue,
-      slideInRight,
-    } = this.props;
-    const className = `container animated ${slideInRight && "slideInRight"}`;
+	render() {
+		const {
+			showPlayer,
+			showAnalyzer: show,
+			showQueue,
+			slideInRight,
+		} = this.props;
+		const className = `container animated ${slideInRight && "slideInRight"}`;
 
-    return (
-      <div>
-        <Nav auth={this.props.auth} dispatch={this.props.dispatch} />
-        <div className={className}>
-          {this.props.children}
-          <Analyzer show={show} />
-        </div>
-        <Containers.Queue show={showQueue} />
-        {showPlayer ? <Containers.Player /> : null}
-        <Containers.Modal />
-        <ToastContainer position="top-right" autoClose={1000} />
-      </div>
-    );
-  }
+		return (
+			<div>
+				<Nav auth={this.props.auth} dispatch={this.props.dispatch} />
+				<div className={className}>
+					{this.props.children}
+					<Analyzer show={show} />
+				</div>
+				<Containers.Queue show={showQueue} />
+				{showPlayer ? <Containers.Player /> : null}
+				<Containers.Modal />
+				<ToastContainer position="top-right" autoClose={1000} />
+			</div>
+		);
+	}
 }
 
 function mapStateToProps({ songData, UIState, auth }) {
-  const { showQueue, showAnalyzer, slideInRight } = UIState;
+	const { showQueue, showAnalyzer, slideInRight } = UIState;
 
-  return {
-    showPlayer: Object.keys(songData.data).length,
-    showAnalyzer,
-    showQueue,
-    slideInRight,
-    auth,
-  };
+	return {
+		showPlayer: Object.keys(songData.data).length,
+		showAnalyzer,
+		showQueue,
+		slideInRight,
+		auth,
+	};
 }
 
 export default connect(mapStateToProps)(App);
