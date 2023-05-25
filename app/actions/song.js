398976ded47974671aa3649a89da27e08550e011
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-05-25 22:41:01 +0700

    commit

--- app/actions/song.js
+++ app/actions/song.js
@@ -7,6 +7,27 @@ import { lrcParser } from "../utils/func";
 export function fetchSong(name, id) {
     return (dispatch) => {
         dispatch({ type: types.START_FETCHING_SONG });
+        axios.get(`/api/media/song?name=${name}&id=${id}`)
+            .then(response => {
+                let data = response.data;
+                axios
+                    .get(data.lyric)
+                    .then(({ data: lrcString }) => {
+                        data.lyric = lrcParser(lrcString).scripts;
+                    })
+                    .catch((err) => console.log(err));
+                data.cover = data.artist ? data.artist.cover : "";
+                const ids = {
+                    songId: data.id,
+                    artistId: data.artist.id,
+                };
+                dispatch(fetchSuggestedSongs(ids));
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+        dispatch(fetchSuggestedSongs(ids));
     }
 }
 
