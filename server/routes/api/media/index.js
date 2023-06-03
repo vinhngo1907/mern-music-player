a859b448d6dd5bb09040a8fe7884c6badbff036a
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-03 14:42:56 +0700

    [api] - artist

--- server/routes/api/media/index.js
+++ server/routes/api/media/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const getSong = require("./song");
 const getTop100 = require("./top100");
 const getArtists = require("./artists");
+const getArtist = require("./artist");
 
 router.get("/song", getSong);
 router.get('/top100/:type', getTop100);
 router.get('/artists', getArtists);
+router.get('/artist/:name/:type', getArtist);
 
 module.exports = router;
