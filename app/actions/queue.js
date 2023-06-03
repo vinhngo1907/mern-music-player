75ea3788f8be5134b192c16ea1035b447802f648
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-03 20:06:04 +0700

    commit

--- app/actions/queue.js
+++ app/actions/queue.js
@@ -3,6 +3,18 @@ import { changeAlias, isEmpty, removeById } from "../utils/func";
 import { fetchSong } from "./song";
 
 
-export function addSongToQueue(){
+export function addSongToQueue() {
+
+}
+
+export function removeSongFromQueue(id) {
+
+}
+
+export function togglePushRoute(bool) {
+
+}
+
+export function tweekSong(songs) {
 
 }
