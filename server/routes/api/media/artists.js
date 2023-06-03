7d6d4d43e04dfa99ce0f2a009e97ffa954699c9f
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-03 13:58:05 +0700

    [PageScrapper] - lib/PageScraper

---
+++ server/routes/api/media/artists.js
@@ -0,0 +1,22 @@
+const { request, pageQuery } = require("utils");
+const Scraper = require("lib/PageScraper");
+const { V_API } = require("consts");
+
+module.exports = function (req, res, next) {
+    const { genre, id, page } = req.query;
+
+    const start = page ? (page - 1) * 20 : 0;
+    request(`http://mp3.zing.vn/the-loai-nghe-si/${genre}/${id}.html?${pageQuery(page)}`)
+        .then(html => {
+            const parser = new Scraper();
+            parser
+                .list('.pone-of-five .item')
+                .setKey('artist')
+                .extractAttr('src', 'img', 'thumb')
+                .extractAttr('text', 'a.txt-primary', 'name')
+                .extractAttr('href', 'a', 'link')
+                .paginate();
+
+            res.json(parser.get());
+        }).catch(error => next(error))
+}
--- server/routes/api/media/index.js
+++ server/routes/api/media/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const getSong = require("./song");
 const getTop100 = require("./top100");
+const getArtists = require("./artists");
 
 router.get("/song", getSong);
 router.get('/top100/:type', getTop100);
+router.get('/artists', getArtists);
 
 module.exports = router;
