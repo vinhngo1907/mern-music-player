5b3abdb7650fdbd066ff2d084be08f7daf28d949
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-15 16:02:38 +0700

    isAuthenticated

--- app/HOC/IsAuthenticated.js
+++ app/HOC/IsAuthenticated.js
@@ -3,10 +3,33 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 export default function (ComposedComponent) {
-    class IsAuthenticated extends React.Component {}
-    
+    class IsAuthenticated extends React.Component {
+
+        static contextType = {
+            router: PropTypes.object
+        }
+
+        redirectTo(route) {
+            this.context.router.push(route);
+        }
+
+        render() {
+            return <ComposedComponent
+                redirectTo={this.redirectTo.bind(this)}
+                {...this.props}
+            />
+        }
+    }
+    function mapStateToProps({ auth }) {
+        const { authenticated, user } = auth;
+
+        return { authenticated, user };
+    }
+
     IsAuthenticated.propTypes = {
         authenticated: PropTypes.bool.isRequired,
         user: PropTypes.object
     }
+
+    return connect(mapStateToProps)(IsAuthenticated);
 }
