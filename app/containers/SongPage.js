4b58c9154c96276028eba1e87041728582d3514c
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-17 21:59:25 +0700

    commit

--- app/containers/SongPage.js
+++ app/containers/SongPage.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux';
 // import { Karaoke as KarokeContainer } from './';
 import { Pages } from '../components';
 import { fetchSong, fetchSuggestedSongs, download } from '../actions/song';
-// import { addSongToStoreTemporarily } from '../actions/user_playlist';
-// import { showAnalyzer, toggleModal } from '../actions/ui';
+import { addSongToStoreTemporarily } from '../actions/user_playlist';
+import { showAnalyzer, toggleModal } from '../actions/ui';
 import { getSongUrl, isEmpty } from '../utils/func';
 
 
@@ -24,7 +24,24 @@ class SongPage extends React.Component {
         const { routing: { localtionBeforeTransitions: currLoc } } = this.props;
         const { routing: { localtionBeforeTransitions: nextLoc } } = this.nextProps;
         const { id: nextId, name } = nextProps.songData;
+        if (nextProps.params.id === nextId) {
+            return;
+        }
 
+        if (nextProps.canPushRoute && nextId !== this.props.songData.id) {
+            browserHistory.push(getSongUrl(name, nextId));
+            return;
+        }
+
+        if (((!currLoc && nextLoc) ||
+            (currLoc && nextLoc && currLoc.pathname !== nextLoc.pathname)) && /song\/.+/.test(nextLoc.pathname)
+        ) {
+            if (this.props.params.id !== nextId) {
+                return;
+            }
+            const { name, id } = nextProps.params;
+            this.props.fetchSong(name, id);
+        }
     }
 
     render() {
@@ -37,9 +54,28 @@ class SongPage extends React.Component {
                     toggleModal={this.props.toggleModal}
                     addSongToStoreTemporarily={this.props.addSongToStoreTemporarily}
                 />
+                <Pages.SongPageBody suggestedSongs={this.props.suggestedSongs} />
             </div>
-        )
+        );
     }
 }
 
-export default SongPage;
+function mapStateToProps(state) {
+    return {
+        suggestedSongs: state.songData.suggestedSongs,
+        songData: state.songData.data,
+        downloadProgress: state.UIState.downloadProgress,
+        routing: state.routing,
+        canPushRoute: state.queueState.pushRoute,
+    };
+}
+
+export default connect(mapStateToProps,
+    {
+        fetchSong,
+        // showAnalyzer,
+        fetchSuggestedSongs,
+        download,
+        addSongToStoreTemporarily,
+        toggleModal,
+    })(SongPage);
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -5,6 +5,7 @@ import UIReducer from './uiReducer';
 import queueReducer from './queueReducer';
 import trackReducer from './trackReducer';
 import chartReducer from './chartReducer';
+import songReducer from './songReducer';
 
 export default combineReducers({
     routing: routerReducer,
@@ -12,5 +13,6 @@ export default combineReducers({
     UIState: UIReducer,
     queueState: queueReducer,
     trackState: trackReducer,
-    chartState: chartReducer
+    chartState: chartReducer,
+    songData: songReducer 
 })
