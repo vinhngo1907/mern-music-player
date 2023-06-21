48f386ac760764da2f0a697dace47564be6aa7d6
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-21 12:46:09 +0700

    top result

---
+++ app/components/SearchMenu/TopResult.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router';
+// import { changeAlias } from '../../utils/func';
+
+function TopResult({ alias, title, link, name, encodeId, artistsNames, objectType, clearSearchResult, thumbnail, thumbnailM }) {
+    const thumbnailTop = objectType === "song" ? thumbnailM : thumbnail;
+    return (
+        <ul className='top-result'>
+            <div className='search-li-title'>
+                Top Result
+            </div>
+            <li>
+                <div className='search-li-detail'>
+                    <img src={thumbnailTop} alt='' />
+                    <div className='search-li-info'>
+                        <div>
+
+                        </div>
+                        <div className='search-li-artist'>
+                            {artistsNames}
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    )
+}
+
+export default TopResult;
--- app/components/SearchMenu/index.js
+++ app/components/SearchMenu/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import onClickOutside from 'react-onclickoutside';
 import SongResult from "./SongResult";
+import ArtistResult from "./ArtistResult";
+import AlbumResult from "./AlbumResult";
 import './index.sass';
 
 class SearchMenu extends Component {
@@ -11,10 +13,12 @@ class SearchMenu extends Component {
   render() {
     const { data } = this.props.searchResult;
     if(!data) return null;
-    
+
     return (
       <ul className='search-menu'>
         <SongResult songs={data.songs || []} clearSearchResult={this.props.clearSearchResult} />
+        <ArtistResult artists={data.artists || []} clearSearchResult={this.props.clearSearchResult} />
+        <AlbumResult albums={data.playlists || []} clearSearchResult={this.props.clearSearchResult} />
       </ul>
     )
   }
