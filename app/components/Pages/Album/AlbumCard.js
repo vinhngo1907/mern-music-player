cdc9019d51e949eece77273a5476599e35cfa0d8
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-22 20:33:08 +0700

    album sass

---
+++ app/components/Pages/Album/AlbumCard.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Link } from 'react-router';
+import LazyloadImage from '../../LazyLoadImage';
+import LinksByComma from '../../LinksByComma';
+import './album_card.sass';
