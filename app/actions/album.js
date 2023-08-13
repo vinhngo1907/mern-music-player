ae2ec1be6d524d3bf17a92c4a3e0607bad2c92d1
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-08-13 13:31:03 +0700

    fetch album

--- app/actions/album.js
+++ app/actions/album.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import * as types from '../constant/action_constant';
 import { startLoading, finishLoading } from './ui';
+import { MEDIA_ENDPOINT } from '../constant/endpoint_constant';
 
 export function setPageNumber(numberOfPages) {
     return {
@@ -22,9 +23,46 @@ export function changePageChunkIndex(pageChunkIndex) {
     };
 }
 
-export function changePageChunkIndex(pageChunkIndex) {
-    return {
-        type: types.CHANGE_PAGE_CHUNK_INDEX,
-        pageChunkIndex,
+export function fetchDefaultAlbums() {
+    return dispatch => {
+        dispatch(startLoading());
+
+        axios.get(`${MEDIA_ENDPOINT}/albums/default`)
+            .then(({ data }) => {
+                console.log(data);
+                if (data) {
+                    dispatch({ type: types.FETCH_DEFAULT_ALBUMS, defaultAlbums: data });
+                    dispatch(clearAlbums()); // clear the albums data
+                    dispatch(finishLoading());
+                }
+            })
+            .catch(error => {
+                dispatch(finishLoading());
+                throw error;
+            });
     };
+}
+
+export function fetchAlbums(genre, id, page) {
+    const pageQuery = page ? `&page=${page}` : '';
+    return dispatch => {
+        dispatch(startLoading());
+        axios.get(`${MEDIA_ENDPOINT}/albums?genre=${genre}&id=${id}${pageQuery}`)
+            .then(({ data }) => {
+                console.log(data);
+                if (data.items && data.items.length) {
+                    dispatch({ type: types.FETCH_ALBUMS, defaultAlbums: data.items });
+                    dispatch(setNumberOfPages(Math.round(data.total / 20)));
+                    dispatch(finishLoading());
+                }
+            })
+            .catch(error => {
+                dispatch(finishLoading());
+                throw error;
+            });
+    }
+}
+
+export function clearPlaylist() {
+    return { type: types.CLEAR_PLAYLIST };
 }
