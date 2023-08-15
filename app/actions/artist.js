a1c3169c5b389413cec150e7ff3b9b499ce435e9
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-08-15 10:41:42 +0700

    artist action

--- app/actions/artist.js
+++ app/actions/artist.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import * as types from '../constant/action_constant';
 import { startLoading, finishLoading } from './ui';
+import { MEDIA_ENDPOINT } from '../constant/endpoint_constant';
 
-export function setPageNumber(numberOfPages) {
+export function setNumberOfPages(numberOfPages) {
     return {
         type: types.SET_NUMBER_OF_PAGES,
         numberOfPages
@@ -22,10 +23,75 @@ export function changePageChunkIndex(pageChunkIndex) {
     };
 }
 
-export function fetchDefaultArtists(){
+export function fetchDefaultArtists() {
+    return dispatch => {
+        dispatch(startLoading());
 
+        axios.get(`${MEDIA_ENDPOINT}/artists/default`)
+            .then(({ data }) => {
+                console.log(data);
+                if (data) {
+                    dispatch({ type: types.FETCH_DEFAULT_ARTISTS, defaultAlbums: data });
+                    dispatch(clearArtists()); // clear the albums data
+                    dispatch(finishLoading());
+                }
+            })
+            .catch(error => {
+                dispatch(finishLoading());
+                throw error;
+            });
+    };
 }
 
-export function fetchArtists(genre, id, page){
-    
+export function fetchArtists(name, type = 'songs', page) {
+    const pageQuery = page ? `?page=${page}` : '';
+    return dispatch => {
+        axios.get(`${MEDIA_ENDPOINT}/artist/${name}/${type}${pageQuery}`)
+            .then(({ data }) => {
+                switch (type) {
+                    case 'songs':
+                        dispatch(fetchSong(data));
+                        break;
+                    case 'albums':
+                        dispatch(fetchAlbum(data));
+                        break;
+                    case 'biography':
+                        dispatch(fetchBio(data));
+                        break;
+                    default:
+                        break;
+                }
+            })
+            .catch(error => {
+                dispatch(finishLoading());
+                throw error;
+            })
+    }
+}
+
+export function fetchAlbum(data) {
+    return {
+        type: types.FETCH_SINGLE_ARTIST_ALBUMS,
+        ...data,
+    };
+}
+
+export function fetchSong(data) {
+    return {
+        type: types.FETCH_SINGLE_ARTIST_SONGS,
+        ...data,
+    };
+}
+
+export function clearArtists() {
+    return {
+        type: types.CLEAR_ARTISTS
+    }
+}
+
+function fetchBio(data) {
+    return {
+        type: types.FETCH_SINGLE_ARTIST_BIOGRAPHY,
+        ...data,
+    };
 }
