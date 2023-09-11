fd0141162a8676371213c17f2cf00ec47cbb2bff
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-11 20:37:07 +0700

    [api] - delete playlist

--- server/routes/api/playlist/delete_playlist.js
+++ server/routes/api/playlist/delete_playlist.js
@@ -1,4 +1,12 @@
 const Playlist = require("models/user_playlist");
 module.exports = (req, res, next) => {
-    const { username } = req.params;
+    const { username, playlistTitle } = req.params;
+
+    Playlist.findOneAndDelete(
+        { _username: username, 'playlists.title': playlistTitle },
+        { $pull: { playlists: { title: playlistTitle } } },
+        { new: true, projection: { 'playlists._id': false } }
+    )
+        .then(doc => res.json(doc.playlists))
+        .catch(err => next(err));
 }
