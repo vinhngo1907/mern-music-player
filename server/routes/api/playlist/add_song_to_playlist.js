db276c43c65e61ea3436b7fd9ff330e1b512bcf8
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-13 20:14:58 +0700

    fix add song to playlist

--- server/routes/api/playlist/add_song_to_playlist.js
+++ server/routes/api/playlist/add_song_to_playlist.js
@@ -4,17 +4,19 @@ const Playlist = require('models/user_playlist');
 module.exports = (req, res, next) => {
     const { username, playlistTitle } = req.params;
     co(function* () {
-        const isSongExisting = yield Playlist.findOne({
-            _username: username,
-            playlists: {
-                $elemMatch: {
-                    title: playlistTitle,
-                    songs: {
-                        $elemMatch: { id: req.body.id }
-                    }
-                }
+        const isSongExisting = yield Playlist.findOne(
+            {
+                _username: username,
+                playlists: {
+                    $elemMatch: {
+                        title: playlistTitle,
+                        songs: {
+                            $elemMatch: { id: req.body.id },
+                        },
+                    },
+                },
             }
-        });
+        );
 
         if (isSongExisting) {
             const error = new Error(`<span>${req.body.title}</span> song already exists in <span>${playlistTitle}</span> playlist`);
@@ -24,11 +26,13 @@ module.exports = (req, res, next) => {
 
         const New = yield Playlist.findOneAndUpdate(
             { _username: username, 'playlists.title': playlistTitle },
-            { $push: { 'playlist.$.songs': req.body } },
+            { $push: { 'playlists.$.songs': req.body } },
+            // upsert add the document to the collection if no result was found
             { safe: true, upsert: true, new: true }
         );
 
         return New;
-    }).then(doc => res.json(doc))
+    })
+        .then(doc => res.json(doc))
         .catch(err => next(err));
-}
+};
