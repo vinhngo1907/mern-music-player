a37f145337d12d2631b08c0686c9407062219f79
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-13 20:05:47 +0700

    album playlist

--- server/routes/api/media/album_playlist.js
+++ server/routes/api/media/album_playlist.js
@@ -3,9 +3,41 @@ const Scraper = require('lib/PageScraper');
 const co = require('co');
 
 module.exports = function getAlbumPlaylist(req, res, next) {
+    const { title, id } = req.query;
+    co(function* () {
+        const html = yield request(`http://mp3.zing.vn/album/${title}/${id}.html`);
+        const regex = /media\/get-source\?type=album&key=.{33}/;
+        const match = html.match(regex);
+        if (!match) throw new Error("can't find the resource URL");
+        const [playlistUrl] = match;
+        const parser = new Scraper(html);
 
+        return yield Promise.all([
+            request(`https://mp3.zing.vn/hrx/${playlistUrl}`),
+            promiseParsing(parser),
+        ]);
+    });
 }
 
 const promiseParsing = (parser) => {
-    
+    console.log({ parser });
+    return new Promise(resolve => {
+        parser
+            .extract('src', '.info-top-play img', 'album_playlist_thumb')
+            .extract('text', '.ctn-inside > h1', 'album_title')
+            .extract('text', '.info-song-top .inline', 'release_year')
+            .extract('text', '.info-artist > h2 > a', 'artist')
+            .extract('src', '.box-artist img', 'artist_thumb')
+            .extract('text', '.artist-info-text > p', 'artist_info')
+            .list('.playlist .fn-song')
+            .setKey('song')
+            .extractAttrs(['href', 'href', 'text'], '.item-song h3 .fn-name', ['id', 'alias', 'title'])
+            .artist('.item-song .fn-artist a');
+        const result = parser.get();
+        result.genres = [];
+        parser.$('.info-song-top').find('a')
+            // prepend album artist to the result
+            .each((index, a) => result.genres.push(parser.$(a).text()));
+        resolve(result);
+    });
 }
